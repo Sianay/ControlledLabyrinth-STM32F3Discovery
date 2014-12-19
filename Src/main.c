63b4408e1b9473bfa558fd4d21308a10cb9cc6da
Arlem2015 <arlem@pc>
2014-12-19 17:38:00 +0100

    debut implementation detection chute

--- Src/main.c
+++ Src/main.c
@@ -155,12 +155,12 @@ int main(void)
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB	, ENABLE);
 	
 	/* Gestion des boutons sur PORT B */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 & GPIO_Pin_1;
+	/*GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 & GPIO_Pin_1;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP; 
-  GPIO_Init(GPIOB, &GPIO_InitStructure);	
+  GPIO_Init(GPIOB, &GPIO_InitStructure);	*/
 	
 	initGPIODetectionChute();
 	initUART();
